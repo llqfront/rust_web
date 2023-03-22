7b1963d5bb7c172ca89460dd5a0122b8368b1a7b
llqfront <llqfront@aliyun.com>
2023-03-22 17:36:47 +0800

    log 注释  release 打包后无法运行

--- rust_axum/common/src/log.rs
+++ rust_axum/common/src/log.rs
@@ -1,6 +1,7 @@
-use log::info;
-use log4rs;
+// use log::info;
+// use log4rs;
 pub fn start_logs() {
-    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
-    info!("booting up");
+    // log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    // info!("booting up");
+    println!("test");
 }
